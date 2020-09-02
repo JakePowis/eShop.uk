d995bde6ca24c97bfba1552ccb3ad4a17369451d
Jake <jakepowis@gmail.com>
2020-09-02 20:32:18 +0100

    adding recomendations

--- frontend/src/screens/WelcomeScreen.js
+++ frontend/src/screens/WelcomeScreen.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import bookImage from '../images/book2.png'
+import dealImage from '../images/loved.jpg'
+import newImage from '../images/sane.jpg'
+import bestImage from '../images/fungi.jpg'
 
 export default function WelcomeScreen(props) {
 
@@ -15,14 +18,20 @@ export default function WelcomeScreen(props) {
 
                 <div className="welcomeHeader">
                     <div className="welcomeContent">
-                        <div className="welcomeText">  enjoy the best books at the cheapest prices<div>only at bookStore</div></div>
+                        <div className="welcomeText">  enjoy the best books at the cheapest prices<div>only at bookStore</div>
+                            <Link to="/shop"><div className="welcomeLink">browse all books</div></Link>
+                        </div>
                         <div> <img style={{ width: "400px", height: "220px" }} src={bookImage} alt="books" /></div>
                     </div>
                 </div>
                 <div className="productRecs">
-                    <div className="welcomeTitle">Todays Deals</div>
-                    <div className="welcomeTitle">Best Seller</div>
-                    <div className="welcomeTitle">New this week</div>
+                    <div className="welcomeTitle">Todays Deals
+                    <Link to="product/"><img className="recImg" src={dealImage} alt="deal" />
+                        </Link></div>
+                    <div className="welcomeTitle">Best Seller
+                    <Link to="product/"> <img className="recImg" src={bestImage} alt="best" /> </Link></div>
+                    <div className="welcomeTitle">New this week
+                    <Link to="product/"><img className="recImg" src={newImage} alt="new" /></Link></div>
                 </div>
                 <div className="welcomeCat">
                     <div className="catTitle">Top Categories</div>
@@ -36,6 +45,6 @@ export default function WelcomeScreen(props) {
 
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
